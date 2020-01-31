336149b3d1e536b241912b05210fa896df31fd87
Higo Ribeiro <HORBJN@gmail.com>
2020-01-31 15:15:59 -0300

    chore: Add bull's listen command

---
+++ providers/Command.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { ServiceProvider } = require('@adonisjs/fold')
+
+class MigrationsProvider extends ServiceProvider {
+  register () {
+    this.app.bind('Rocketseat/Commands/Bull:Listen', () => require('../src/Commands/Listen'))
+  }
+
+  boot () {
+    const ace = require('@adonisjs/ace')
+    console.log('COmando')
+    ace.addCommand('Rocketseat/Commands/Bull:Listen')
+  }
+}
+
+module.exports = MigrationsProvider
